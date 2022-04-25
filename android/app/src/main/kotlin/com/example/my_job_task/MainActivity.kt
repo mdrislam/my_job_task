864e318f5dd6d5aff6431fd57edc610c28b27fc1
Risad Hossain Home <mmdrislam@gmail.com>
2022-04-25 19:12:03 +0600

    init this project

---
+++ android/app/src/main/kotlin/com/example/my_job_task/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.my_job_task
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ui/navigation/bottom_navigation.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BottomNavigation extends StatefulWidget {
+  const BottomNavigation({Key? key}) : super(key: key);
+
+  @override
+  State<BottomNavigation> createState() => _BottomNavigationState();
+}
+
+class _BottomNavigationState extends State<BottomNavigation> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
